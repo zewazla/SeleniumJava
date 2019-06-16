f6f77caecebae0a7c4a02b2a80046aae271afedc
zewazla <zewazla@gmail.com>
2019-06-16 18:23:19 +0100

    radio button dynamically

---
+++ SeleniumJavaPractice/src/test/java/selenium/test/project/autoSuggestive/Radio_Buttons.java
@@ -0,0 +1,35 @@
+package selenium.test.project.autoSuggestive;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Dropdown_Dynamic_AutoSugestive {
+
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\Lenovo\\Desktop\\chromedriver_win32\\chromedriver.exe");
+        WebDriver driver =new ChromeDriver();
+        driver.get("https://www.fly.com/");
+
+        WebElement from = driver.findElement(By.id("searchinput-from"));
+
+        from.sendKeys("MUMBAI");
+        Thread.sleep(2000);
+        from.sendKeys(Keys.ENTER);
+
+
+        WebElement to = driver.findElement(By.id("searchinput-to"));
+
+        to.sendKeys("DEL");
+        Thread.sleep(2000);
+
+        for(int i = 1; i<5; i++){
+            to.sendKeys(Keys.ARROW_DOWN);
+        }
+        to.sendKeys(Keys.ENTER);
+
+    }
+
+}
